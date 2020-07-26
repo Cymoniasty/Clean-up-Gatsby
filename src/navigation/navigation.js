0db7c8f5cf8f4527319767d4665f3370d90f9c7c
Damian Cybiak <d.cybiak@softwizard.pl>
2020-07-27 00:56:32 +0200

    First version animation hamburger

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import styled, {createGlobalStyle} from "styled-components";
-import {FaSearch} from 'react-icons/fa';
-import {FaHeart} from 'react-icons/fa';
+import {FaHeart, FaSearch} from 'react-icons/fa';
 
 const GlobalStyle = createGlobalStyle`
     * {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
+    }
+    
+    body{
+    background-color: #333;
     }
 `;
 
@@ -18,8 +21,10 @@ const NavigationAside = styled.aside`
     right: 0;
     background-color: #0E0E0E;
     width: 260px;
+    transform: ${({showNav}) => showNav ? "translateX(0)" : "translateX(260px)"};
+    transition: transform .2s linear;
 
-`
+`;
 
 const NavigationWrapper = styled.nav`
 
@@ -35,26 +40,105 @@ ul{
 
 }`
 
-const Navigation = () => (
-    <>
-        <GlobalStyle/>
-        <NavigationAside>
-            <NavigationWrapper>
-                <ul>
-                    <li>Szukaj
-                        <FaSearch/>
-                    </li>
-                    <li>Kontakt</li>
-                    <li>O nas</li>
-                    <li>Inne strony</li>
-                    <li>Wesprzyj
-                        <FaHeart/>
-                    </li>
-                    <li>Język: PL</li>
-                </ul>
-            </NavigationWrapper>
-        </NavigationAside>
-    </>
-);
+const NavigationHamburger = styled.button`
+
+    position: fixed;
+    top: 25px;
+    right: 25px;
+    transition: .2s linear;
+    cursor: pointer;
+    background-color: transparent;
+    z-index: 1;
+    border: none;
+    display: inline-block;
+`
+
+const NavigationHamburgerBox = styled.span`
+
+  width: 24px;
+  height: 20px;
+  display: inline-block;
+  position: relative;
+
+`
+
+const HamburgerInner = styled.span`
+    width: 100%;
+    height: 3px;
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    background-color: ${({hamburger}) => hamburger ? 'transparent' : '#fff'};
+    transition: background-color .2s linear;
+    border-radius:25px;
+    &::before{
+      content: '';
+      top: -8px;
+      transition: background-color .2s linear, transform .2s;
+      transform: ${({hamburger}) => hamburger ? 'translateY(8px) rotate(135deg)' : "translateY(0) rotate(0)"};
+      right: 0;
+      width: 50%;
+      height: 3px;
+      border-radius:25px;
+      position: absolute;
+      background-color: ${({hamburger}) => hamburger ? '#fff' : '#fff'};
+    
+    }
+    &::after{
+      content: '';
+      top: 8px;
+      border-radius:25px;
+      right: 0;
+      width: 70%;
+      float: right;
+      text-align: right;
+      height: 3px;
+      position: absolute;
+      transition: background-color .2s linear, transform .2s linear;
+      transform: ${({hamburger}) => hamburger ? 'translateY(-8px) rotate(-135deg)' : "translateY(0) rotate(0)"};
+      background-color: ${({hamburger}) => hamburger ? '#fff' : '#fff'};
+
+    }
+`
+
+const Navigation = () => {
+    const [menu, showMenu] = useState(false)
+
+    return (
+        <>
+            <GlobalStyle/>
+
+            <NavigationHamburger hamburger={menu} onClick={() => showMenu(!menu)}>
+
+                <NavigationHamburgerBox>
+
+                    <HamburgerInner hamburger={menu}/>
+
+                </NavigationHamburgerBox>
+
+            </NavigationHamburger>
+
+            <NavigationAside showNav={menu}>
+
+                <NavigationWrapper>
+                    <ul>
+                        <li>Szukaj
+                            <FaSearch/>
+                        </li>
+                        <li>Kontakt</li>
+                        <li>O nas</li>
+                        <li>Inne strony</li>
+                        <li>Wesprzyj
+                            <FaHeart/>
+                        </li>
+                        <li>Język: PL</li>
+                    </ul>
+                </NavigationWrapper>
+            </NavigationAside>
+        </>
+    )
+};
 
 export default Navigation;
