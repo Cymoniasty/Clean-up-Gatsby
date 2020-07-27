761a1ca1edcb92cd5327b746487f8f745174a430
Damian Cybiak <d.cybiak@softwizard.pl>
2020-07-27 16:01:03 +0200

    clean up navigation.js

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import styled, {createGlobalStyle} from "styled-components";
-import {FaHeart, FaSearch} from 'react-icons/fa';
+import {FaChevronDown, FaSearch, FaHeart} from 'react-icons/fa';
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -10,134 +10,114 @@ const GlobalStyle = createGlobalStyle`
     }
     
     body{
-    background-color: #333;
+      background-color: #333;
     }
     
     svg{
-    transform: translateY(20%);
-    margin-left: 10px;
-
+        transform: translateY(20%);
+        margin-left: 10px;
     }
 `;
 
 const NavigationAside = styled.aside`
+    width: 260px;
     position: fixed;
     top: 0;
     bottom: 0;
     right: 0;
     background-color: #0E0E0E;
-    width: 260px;
     transform: ${({showNav}) => showNav ? "translateX(0)" : "translateX(260px)"};
     transition: transform .2s linear;
-
 `;
 
 const NavigationWrapper = styled.nav`
-
-ul{
-    text-align: right;
-    margin-top: 80px;
-    margin-right: 40px;
-    list-style: none;
-    color: white;
-    font-family: "Muli" ,sans-serif; 
-    font-size: 18px;
-    line-height: 50px;
-
-}`
+    ul{
+        text-align: right;
+        font-size: 18px;
+        margin-top: 80px;
+        margin-right: 40px;
+        line-height: 50px;
+        list-style: none;
+        color: white;
+        font-family: "Muli" ,sans-serif; 
+    }`
 
 const NavigationHamburger = styled.button`
-
     position: fixed;
     top: 25px;
     right: 25px;
-    transition: .2s linear;
-    cursor: pointer;
-    background-color: transparent;
-    z-index: 1;
-    border: none;
     display: inline-block;
+    z-index: 1;
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+    transition: .2s linear;
 `
 
 const NavigationHamburgerBox = styled.span`
-
-  width: 24px;
-  height: 20px;
-  display: inline-block;
-  position: relative;
-
+    position: relative;
+    display: inline-block;
+    width: 24px;
+    height: 20px;
 `
 
 const HamburgerInner = styled.span`
-    width: 100%;
-    height: 3px;
     display: block;
     position: absolute;
-    right: 0;
     top: 50%;
-    transform: translateY(-50%);
-    background-color: ${({hamburger}) => hamburger ? 'transparent' : '#fff'};
-    transition: background-color .2s linear;
+    right: 0;
+    width: 100%;
+    height: 3px;
     border-radius:25px;
+    background-color: ${({hamburger}) => hamburger ? 'transparent' : '#fff'};
+    transform: translateY(-50%);
+    transition: background-color .2s linear;
     &::before{
       content: '';
+      position: absolute;
       top: -8px;
-      width: ${({hamburger}) => hamburger ? '70%' : "50%"};
-      transition: background-color .2s linear, transform .2s;
-      transform: ${({hamburger}) => hamburger ? 'translateY(8px) rotate(135deg)' : "translateY(0) rotate(0)"};
       right: 0;
+      width: ${({hamburger}) => hamburger ? '70%' : "50%"};
       height: 3px;
       border-radius:25px;
-      position: absolute;
       background-color: ${({hamburger}) => hamburger ? '#fff' : '#fff'};
-    
+      transform: ${({hamburger}) => hamburger ? 'translateY(8px) rotate(135deg)' : "translateY(0) rotate(0)"};
+      transition: background-color .2s linear, transform .2s;
     }
     &::after{
       content: '';
+      position: absolute;
       top: 8px;
-      border-radius:25px;
       right: 0;
       width: 70%;
       height: 3px;
-      position: absolute;
-      transition: background-color .2s linear, transform .2s linear;
-      transform: ${({hamburger}) => hamburger ? 'translateY(-8px) rotate(-135deg)' : "translateY(0) rotate(0)"};
+      border-radius:25px;
       background-color: ${({hamburger}) => hamburger ? '#fff' : '#fff'};
-
+      transform: ${({hamburger}) => hamburger ? 'translateY(-8px) rotate(-135deg)' : "translateY(0) rotate(0)"};
+      transition: background-color .2s linear, transform .2s linear;
     }
 `
-
 const Navigation = () => {
     const [menu, showMenu] = useState(false)
-
     return (
         <>
             <GlobalStyle/>
 
             <NavigationHamburger hamburger={menu} onClick={() => showMenu(!menu)}>
-
                 <NavigationHamburgerBox>
-
                     <HamburgerInner hamburger={menu}/>
-
                 </NavigationHamburgerBox>
-
             </NavigationHamburger>
 
             <NavigationAside showNav={menu}>
-
                 <NavigationWrapper>
                     <ul>
-                        <li>Szukaj
-                            <FaSearch/>
-                        </li>
+                        <li>Szukaj<FaSearch/></li>
                         <li>Kontakt</li>
                         <li>O nas</li>
-                        <li>Inne strony</li>
-                        <li>Wesprzyj
-                            <FaHeart/>
-                        </li>
-                        <li>Język: PL</li>
+                        <li>Inne strony<FaChevronDown/></li>
+                        <li>Wesprzyj<FaHeart/></li>
+                        <li>Język: PL<FaChevronDown/></li>
                     </ul>
                 </NavigationWrapper>
             </NavigationAside>
